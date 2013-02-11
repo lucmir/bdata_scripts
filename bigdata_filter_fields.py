e0612925c3aecac652fceb372251361d4016a90b
Lucas Cunha <luccmir@gmail.com>
2013-02-11 18:48:30 -0200

    correção

--- bigdata_filter_fields.py
+++ bigdata_filter_fields.py
@@ -38,9 +38,11 @@ results = collection.find(filters, fields)
 out_file = open(OUT_FILE_PATH, 'w')
 
 for result in results:
-	if 'user_id' in result and 'video_id' in result and 'creation_time' in result and 'last_update_time' in result:
+	
+	if 'user_id' in result and 'video_id' in result and 'creation_time' in result and 'last_update_time' in result and 'video_duration' in result:
 		genre = result['video_genre'] if 'video_genre' in result else 'NONE'
-		line = '%s\t%s\t%s\t%s\t%s\n' % (result['user_id'], result['video_id'], result['creation_time'], result['last_update_time'], genre)
+		line = '%s\t%s\t%s\t%s\t%s\t%s\n' % (result['user_id'], result['video_id'], \
+			result['creation_time'], result['last_update_time'], genre, result['video_duration'])
 		out_file.write(line)
 
 out_file.close()
