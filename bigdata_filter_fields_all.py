48128814ba142d659c2950f2877691c01c4a255c
Lucas Cunha <luccmir@gmail.com>
2013-02-11 22:00:41 -0200

    adicionando video_duration no bigdata_filter_fields_all

--- bigdata_filter_fields_all.py
+++ bigdata_filter_fields_all.py
@@ -26,7 +26,8 @@ fields = {  '_id' : False,
 			'last_update_time' : True,
 			'video_id' : True,
 			'user_id' : True,
-			'video_genre' : True }
+			'video_genre' : True,
+			'video_duration' : True }
 
 filters = {}
 
@@ -37,10 +38,12 @@ results = collection.find(filters, fields)
 out_file = open(OUT_FILE_PATH, 'w')
 
 for result in results:
-	if 'user_id' in result and 'video_id' in result and 'creation_time' in result and 'last_update_time' in result:
+	
+	if 'user_id' in result and 'video_id' in result and 'creation_time' in result and 'last_update_time' in result and 'video_duration' in result:
 		genre = result['video_genre'] if 'video_genre' in result else 'NONE'
-		line = '%s\t%s\t%s\t%s\t%s\n' % (result['user_id'], result['video_id'], result['creation_time'], result['last_update_time'], genre)
+		line = '%s\t%s\t%s\t%s\t%s\t%s\n' % (result['user_id'], result['video_id'], \
+			result['creation_time'], result['last_update_time'], genre, result['video_duration'])
 		out_file.write(line)
 
-out_file.close()
+out_file.close
 
